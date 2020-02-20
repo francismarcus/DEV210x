6cc8f5f4b35611e621ae592c5afe31cd0ea70c03
francismarcus <marcusmagnusson@outlook.com>
2020-02-20 11:55:49 +0100

    Funcs

---
+++ FunctionsAndObjects/FunctionSum.h
@@ -0,0 +1,7 @@
+#ifndef DEV210X_FUNCTIONSUM_H
+#define DEV210X_FUNCTIONSUM_H
+
+int Sum(int a, int b);
+int Sum(int a, int b, int c);
+
+#endif //DEV210X_FUNCTIONSUM_H
---
+++ FunctionsAndObjects/Functions.cpp
@@ -0,0 +1,47 @@
+#include "FunctionSum.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << Sum(2, 5) << endl;
+  cout << Sum(2, 5, 7) << endl;
+  return 0;
+}
+
+int Sum(int a, int b) { return a + b; }
+
+int Sum(int a, int b, int c) { return a + b + c; }
+
+// Inline functions
+inline void swap(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+int main() {
+  int a{5};
+  int b{8};
+  swap(a, b);
+  // note the values are swapped
+  cout << "a: " << a << " b: " << b << endl;
+  return 0;
+}
+
+// StorageClassesAndScope
+int main() {
+    // static keyword - allocated when program starts, deallocated when program ends. Variable will retain its
+    // value between calls to the function.
+    // extern keyword - used to declare an object that is defined in another translation unit within the enclosing
+    // scope but has an external linkage.
+    // thread_local keyword - identifier is only accessible on the thread which it is created. Prevents sharing
+    // of the identifier across multiple threads in same application. (C++ 11 standard)
+  int total{0};
+  for (int i = 0; i <= 10; i++) {
+    total += i;
+  }
+  cout << "The sum of the numbers 1 to 10 is: " << total << endl;
+  // this line will not compile as i is now out of scope.
+  // cout << "Current value of i is: " << i << endl;
+  return 0;
+}
